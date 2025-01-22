e63677381a2892454bd2ab81b13553b1240a431e
namra-dhol <dholnamra@gmail.com>
2025-01-22 20:55:29 +0530

    remove mvc project and add .gitignore file

--- Quiz-Management/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Quiz_Management.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
