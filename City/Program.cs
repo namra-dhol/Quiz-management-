9caa4f253bcfe5a06120f88658f837870d221b6a
namra-dhol <dholnamra@gmail.com>
2025-02-01 10:11:05 +0530

    add formeted proc

--- City/Program.cs
+++ City/Program.cs
@@ -20,8 +20,6 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.MapControllerRoute(
-            name: "areas",
-            pattern: "{area:exists}/{controller=Country}/{action=Index}/{id?}");
+
 
 app.Run();
