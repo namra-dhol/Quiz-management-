fed671652b12f20a0416c6ccae5098084ca6c551
namra-dhol <dholnamra@gmail.com>
2025-02-03 10:37:15 +0530

    address book

--- Quiz/Controllers/QuizController.cs
+++ Quiz/Controllers/QuizController.cs
@@ -1,12 +1,38 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Data.SqlClient;
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 namespace Quiz.Controllers
 {
     public class QuizController : Controller
     {
+        private IConfiguration configuration;
+
+        public QuizController(IConfiguration _configuration)
+        {
+            configuration = _configuration;
+        }
         public IActionResult QuizList()
         {
-            return View();
+            string connectionString = configuration.GetConnectionString("ConnectionString");
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.CommandText = "PR_Product_SelectAll";
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        DataTable table = new DataTable();
+                        table.Load(reader);
+                        return View(table);
+                    }
+                }
+            }
         }
 
         public IActionResult AddQuiz()
---
+++ Quiz/Models/QuizModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Quiz.Models
+{
+    public class QuizModel
+    {
+      
+        [Required]
+        [StringLength(100)]
+        public string QuizName { get; set; }
+
+        [Required]
+        public int TotalQuestions { get; set; }
+
+        [Required]
+        public DateTime QuizDate { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public int UserID { get; set; }
+
+        [Required]
+        public DateTime modified { get; set; }
+
+    }
+}
